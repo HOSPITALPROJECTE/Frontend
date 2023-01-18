379fcbb5bb53c8a3a93650c99f5e73ef730aea55
Liu Santana <lsantana8@boscdelacoma.cat>
2023-01-18 20:58:58 +0100

    Treballador Administradors

--- frontEnd/src/app/project/components/Admin/admin-treballadors/admin-treballadors.component.ts
+++ frontEnd/src/app/project/components/Admin/admin-treballadors/admin-treballadors.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,14 +6,40 @@ import { Router } from '@angular/router';
   templateUrl: './admin-treballadors.component.html',
   styleUrls: ['./admin-treballadors.component.css']
 })
-export class AdminTreballadorsComponent implements OnInit {
+export class AdminTreballadorsComponent implements OnInit, AfterViewInit {
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-
   }
 
+  ngAfterViewInit(): void {
+    this.selectTreballador();
+    this.btnsActions();
+  }
+  selectTreballador(){
+    let treballadors =  document.querySelectorAll('.treballador');
+    treballadors?.forEach(li => li.addEventListener('click', () => {
+      this.removeSelecions(treballadors);
+      li.classList.add('active');
+
+      this.actionsWithTreballador();
+    }))
+  }
+  removeSelecions(treballadors:NodeListOf<Element>){
+    treballadors.forEach(t => t.classList.remove('active'));
+  }
+  actionsWithTreballador(){
+    document.querySelector('.btns')?.classList.add('active');
+  }
+
+  btnsActions(){
+    document.querySelector('.btn_secondary')?.addEventListener('click', () => {
+      document.querySelector('.btns')?.classList.remove('active');
+      this.removeSelecions(document.querySelectorAll('.treballador'));
+    });
+  }
+  /* NAVEGACIO */
   goToAdmin(){
     this.router.navigate(['/admin']);
   }
