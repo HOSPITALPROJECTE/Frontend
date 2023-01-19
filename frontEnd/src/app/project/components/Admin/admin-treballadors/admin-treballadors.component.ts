e145c9b378df7bb475d90c1fca860d1dc20349f9
Liu Santana <lsantana8@boscdelacoma.cat>
2023-01-19 17:08:47 +0100

    filtres treballador

--- frontEnd/src/app/project/components/Admin/admin-treballadors/admin-treballadors.component.ts
+++ frontEnd/src/app/project/components/Admin/admin-treballadors/admin-treballadors.component.ts
@@ -7,15 +7,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./admin-treballadors.component.css']
 })
 export class AdminTreballadorsComponent implements OnInit, AfterViewInit {
-
+  filter_name: string = '';
+  filter_dni: string = '';
+  filter_select!: string;
   constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   ngAfterViewInit(): void {
-    this.selectTreballador();
-    this.btnsActions();
+    this.setFilterSelect()
+    this.selectTreballador(); // acció al seleccionar un treballador
+    this.btnsActions(); // accions per treballador ex: veure guardies
+    this.filterTable(); // filtratje de la taula
+  }
+  setFilterSelect(){
+    let select = document.querySelector('select')?.value;
+    if (select!=null) this.filter_select = select;
+    else this.filter_select = '';
   }
   selectTreballador(){
     let treballadors =  document.querySelectorAll('.treballador');
@@ -39,6 +48,36 @@ export class AdminTreballadorsComponent implements OnInit, AfterViewInit {
       this.removeSelecions(document.querySelectorAll('.treballador'));
     });
   }
+  filterTable(){
+    let list = document.querySelectorAll('.treballador');
+    document.querySelector('.filtres > button')?.addEventListener('click', () => {
+      this.filterElements(list);
+    });
+  }
+  filterElements(list:NodeListOf<Element>){
+    list.forEach(l => {
+      if(this.filterTrue(l))l.classList.remove('hide');
+      else l.classList.add('hide');
+    });
+  }
+  filterTrue(element:Element){
+    let select = element.querySelector("p[name='cat']")?.textContent == this.filter_select;
+    let name = element.querySelector("p[name='nom']")?.textContent?.includes(this.filter_name);
+    let dni = element.querySelector("p[name='dni']")?.textContent?.includes(this.filter_dni);
+    return select && dni && name;
+  }
+
+
+
+	setSelectFilter(value:string): void {
+		this.filter_select = value;
+	}
+	setNomFilter(value:string): void {
+		this.filter_name = value;
+	}
+	setDniFilter(value:string): void {
+		this.filter_dni = value;
+	}
   /* NAVEGACIO */
   goToAdmin(){
     this.router.navigate(['/admin']);
