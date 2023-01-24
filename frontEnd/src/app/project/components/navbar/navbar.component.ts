674fc7a3c6b7aaf314b52cbacbaa1c92b3095c08
grabu6 <mgrabulosa071@boscdelacoma.cat>
2023-01-24 16:17:01 +0100

    admin o user control de navbar

--- frontEnd/src/app/project/components/navbar/navbar.component.ts
+++ frontEnd/src/app/project/components/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
+import { Token } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import jwt_decode from "jwt-decode";
+
 
 @Component({
   selector: 'app-navbar',
@@ -7,10 +10,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isAdmin!:boolean;
+  
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+    
+    const token = localStorage.getItem("hospital-accessToken");
+    console.log(token);
+    if(token!=null){
+    const decodedToken:any = jwt_decode(token);
+    console.log(decodedToken);
+    this.isAdmin = decodedToken.rol === 'ADMIN';
+    console.log(this.isAdmin)
+    }
   }
   goToCalendari(){
     this.router.navigate(['/month']);
