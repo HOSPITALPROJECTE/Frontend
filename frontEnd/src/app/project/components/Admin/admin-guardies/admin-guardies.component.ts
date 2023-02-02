9582a5654ae02c33bf1d80fce11075c743d4e5a0
grabu6 <mgrabulosa071@boscdelacoma.cat>
2023-02-02 16:41:05 +0100

    filtratge per data

--- frontEnd/src/app/project/components/Admin/admin-guardies/admin-guardies.component.ts
+++ frontEnd/src/app/project/components/Admin/admin-guardies/admin-guardies.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { STRING_TYPE } from '@angular/compiler';
+import { AfterViewInit, Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ATreballador } from 'src/app/project/services/api/treballador/ATreballador';
 
@@ -8,13 +9,13 @@ import { ATreballador } from 'src/app/project/services/api/treballador/ATreballa
   templateUrl: './admin-guardies.component.html',
   styleUrls: ['./admin-guardies.component.css']
 })
-export class AdminGuardiesComponent{
+export class AdminGuardiesComponent implements AfterViewInit{
 
-  data!:Array<string>;
+  data:any;
   unitats:Array<string> =['Unitat 1','Unitat 2','Unitat 3', 'Unitat 4']
-  unitat:string='Unitat 1';
+  unitat:string='Totes';
   categories:Array<string> =['Infermeria','TCAI']
-  categoria:string='Infermeria';
+  categoria:string='Totes';
   torns!:Array<any>;
   places!:Array<any>;
 
@@ -26,21 +27,50 @@ export class AdminGuardiesComponent{
       this.guardies = data['resultat']['dades'];
     })
   }
-  setFilterSelectUnitat(){
-    let select = document.querySelector('select')?.value[0];
-    if (select!=null) this.unitat = select;
-    else this.unitat = 'Unitat 1';
+
+  ngAfterViewInit(): void {
+    this.btnsActions(); 
   }
 
-  setFilterSelectCategoria(){
-    let select = document.querySelector('select')?.value[1];
-    if (select!=null) this.categoria = select;
-    else this.categoria = 'Infermeria';
+  selectGuardia(li:Element){
+    let ul = li.parentNode?.querySelectorAll('.table_li');
+    if(ul != null) this.removeSelecions(ul);
+    li.classList.add('active');
+
+    this.actionsWithGuardia();
   }
   removeSelecions(guardies:NodeListOf<Element>){
     guardies.forEach(t => {
       t.classList.remove('active');
       document.querySelector('#exit')?.classList.remove('hide');
+    });
+  }
+  setFilterSelectUnitat(){
+    let select = document.querySelector('select')?.value[0];
+    if (select!=null) this.unitat = select;
+    else this.unitat = 'Unitat 1';
+  }
+  setFilterSelectCategoria(){
+    let select = document.querySelector('select')?.value[1];
+    if (select!=null) this.categoria = select;
+    else this.categoria = 'Infermeria';
+  }
+
+  btnsActions(){
+    this.btnSecondary();
+    this.btnPrimary();
+  }
+  btnPrimary(){
+    document.querySelector('.btn_primary')?.addEventListener('click', () => {
+      let unitat = document.querySelector(".table_li.active > p[class='unitat']")?.textContent;
+      if(unitat != null)
+        this.goToTreballadorsGuardia()
+    })
+  }
+  btnSecondary(){
+    document.querySelector('.btn_secondary')?.addEventListener('click', () => {
+      document.querySelector('.btns')?.classList.remove('active');
+      this.removeSelecions(document.querySelectorAll('.table_li'));
     });
   }
   actionsWithGuardia(){
@@ -57,14 +87,16 @@ export class AdminGuardiesComponent{
     });
   }
   filterTrue(element:Element){
-    /*let data = element.querySelector("p[class='dia']")?.textContent == this.data;*/
-    let unitat = element.querySelector("p[class='unitat']")?.textContent == this.unitat;
-    let categoria = element.querySelector("p[class='categoria']")?.textContent == this.categoria;
-    return  unitat && categoria;
+    let data = element.querySelector("p[class='dia']")?.textContent == this.data;
+    let unitat = element.querySelector("p[class='unitat']")?.textContent == this.unitat|| this.unitat == 'Totes'
+    let categoria = element.querySelector("p[class='categoria']")?.textContent == this.categoria|| this.categoria == 'Totes'
+    return data && unitat && categoria;
   }
 
-  setSelectFilterDate(data:any): void {
-		this.data = data;
+  setSelectFilterDate(data:string): void {
+    let date=this.dataToString(data);
+    this.data=date;
+    console.log(this.data);
 	}
   setSelectFilterCategoria(categoria:string): void {
 		this.categoria = categoria;
@@ -72,9 +104,14 @@ export class AdminGuardiesComponent{
   setSelectFilterUnitat(unitat:string): void {
 		this.unitat = unitat;
 	}
-  dataToString(data:Date){
+  dataToString(data:string){
     return new DatePipe("en-US").transform(data, "dd-MM-yyyy");
   }
+  
+  goToTreballadorsGuardia(){
+    this.router.navigate(['/admin-treballadors-x-guardia']);
+  }
+
   goToAdmin(){
     this.router.navigate(['/admin']);
   }
